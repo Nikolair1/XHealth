10c91b2f43cbe51b4885b17984c01f7c8d54250d
Nikolai Ramalingam <NIKOLAIRAMALINGAM@GMAIL.COM>
2024-04-21 00:27:36 -0700

    Debugging prompts

--- post_tweets.py
+++ post_tweets.py
@@ -32,10 +32,14 @@ topics = [
 
 # topics is a dicitonary from topic -> topic summary
 def tweet(summary, topic_summaries):
-    text = "2Weekly Summary 💊🏥🩺📋 \n" + summary + "\n\nTopics:\n"
-    for topic in topics:
-        text += topic + "\n"
+    print("ARGS ARE ", summary)
+    print(len(summary))
 
+    text = "Weekly Summary 📋 \n"
+    for topic, emoji in zip(summary, topics):
+        text += emoji[-1] + " " + topic + "\n"
+
+    summary_later = text
     response = client.create_tweet(
         text=text,
         user_auth=True,
@@ -45,6 +49,7 @@ def tweet(summary, topic_summaries):
 
     counter = 0
     while counter < 4:
+        print("Length of topic is ", len(topic_summaries[counter]))
         client.create_tweet(
             text=topics[counter][-1] + " " + topic_summaries[counter],
             user_auth=True,
@@ -56,13 +61,13 @@ def tweet(summary, topic_summaries):
     flask_endpoint = "http://127.0.0.1:5000/add_tweet"
     # Construct the data dictionary
     data = {
-        "summary": summary,
+        "summary": summary_later,
         "topic1": topic_summaries[0],  # Assign the first topic summary to topic1
         "topic2": topic_summaries[1],  # Assign the second topic summary to topic2
         "topic3": topic_summaries[2],  # Assign the third topic summary to topic3
         "topic4": topic_summaries[3],  # Assign the fourth topic summary to topic4
         "link": "https://twitter.com/_nicky_2/status/" + response.data["id"],
-        "date": "2024-06-12",
+        "date": "2024-06-13",
     }
     response = requests.post(flask_endpoint, json=data)
     if response.status_code == 200:
@@ -72,13 +77,13 @@ def tweet(summary, topic_summaries):
 
 
 # testing
-summary = (
-    "Our first summary focuses on Bird Flu and good advice from reputable doctors."
-)
-topic_summaries = [
-    "1Eat an apple a day!",
-    "1Measles is at an all time low!",
-    "1Bird flu affects different social classes differently.",
-    "1Researchers found huge potential of weight-loss drugs like Ozempic and Wegovy, and their implications for public health and medical research.",
-]
-tweet(summary, topic_summaries)
+# summary = (
+#     "Our first summary focuses on Bird Flu and good advice from reputable doctors."
+# )
+# topic_summaries = [
+#     "1Eat an apple a day!",
+#     "1Measles is at an all time low!",
+#     "1Bird flu affects different social classes differently.",
+#     "1Researchers found huge potential of weight-loss drugs like Ozempic and Wegovy, and their implications for public health and medical research.",
+# ]
+# tweet(summary, topic_summaries)
